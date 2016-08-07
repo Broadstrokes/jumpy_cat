6c60b9ffc49a5e14e857b652f9cb172dfe3496a7
Dan Sajjad <dan.sajjad@gmail.com>
2016-08-07 14:46:28 -0700

    Add create, list, delete methods on frontend & backend, added routes

--- public/meetups-controller.js
+++ public/meetups-controller.js
@@ -1,13 +1,14 @@
 app.controller('meetupsCtrl', function($scope, $http, $resource) { //init controller
-
   var Meetup = $resource('/api/meetups');
 
-  $scope.meetups = [
-    {name: 'Bikers SF'},
-    {name: 'Football SF'}
-  ];
+  $scope.meetups = [];
+  $scope.refresh = function (arguments) {
+    Meetup.query(function(results) {
+      $scope.meetups = results;
+    });    
+  }
 
-  $scope.meetupsCount = $scope.meetups.length;
+  $scope.refresh();
   $scope.createMeetup = function() {
     // $scope.meetups.push({name: $scope.meetupName});
     // $scope.meetupName = "";
@@ -16,8 +17,19 @@ app.controller('meetupsCtrl', function($scope, $http, $resource) { //init contro
     meetup.name = $scope.meetupName;
     console.log(meetup.name);
     $scope.meetupName = "";
-    meetup.$save();
-
+    meetup.$save(function (result) {
+      $scope.meetups.push(result);
+    });
   };
+
+  $scope.deleteMeetup = function(meetup) {
+    console.log('Hello I am here. meetup is: ', meetup._id);
+    var Meetup = $resource('/api/meetups/' + meetup._id);
+    Meetup.remove(function (result) {
+      console.log(result.message);   
+      $scope.refresh();
+    });
+  }
+
 
 });
--- server.js
+++ server.js
@@ -1,19 +1,65 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+//Connect to MongoDB
+mongoose.connect('mongodb://localhost/mikemoser-mean', function() {
+  console.log('Connected to DB');
+});
+
+//Meetup model
+// var mongoose = require('mongoose');
+
+  //Create Schema
+  var meetupSchema = mongoose.Schema({
+    name: {
+      type: String, 
+      required: true
+    }
+  });
+
+  //Compiling schema into Model
+  var Meetup = mongoose.model('Meetup', meetupSchema);
+
+//Meetup controller
+var MeetupCtrl = {};
+MeetupCtrl.list = function (req, res) {
+  Meetup.find({}, function (err, results) {
+    res.json(results);
+  });
+}
+
+MeetupCtrl.create = function (req, res) {
+  var meetup = new Meetup(req.body);
+  meetup.save(function(err, result) {
+    if(err) {console.log('Could not save to db');}
+    else {
+      console.log('Saved ', req.body, ' to db!'); 
+      res.json(result);
+    }
+  });
+}
+
+MeetupCtrl.delete = function (req, res) {
+  console.log('Hitting delete');
+  Meetup.remove({ _id:req.params.id }, function (err, results) {
+    if(err) {console.log('Something went wrong');}
+    else {
+      console.log('Deleted ', ' from db!'); 
+      res.json({message: 'Successfully Deleted'});
+    }
+  });
+}
 
 //Middleware
 app.use(bodyParser());
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  console.log('Serving request for ' + req.url);
-  res.render('index');
-});
-
-app.post('/api/meetups', function(req, res) {
-  console.log(req.body);
-  res.json();
-});
+app.get('/', function(req, res) { res.render('index'); });
+app.get('/api/meetups', MeetupCtrl.list);
+app.post('/api/meetups', MeetupCtrl.create);
+app.delete('/api/meetups/:id', MeetupCtrl.delete);
 
 app.listen(3000, function() { console.log('Listening on 3000')});
