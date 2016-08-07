fc0e8c350aa6e75dc031ecc8ef2be6c1a76e63c7
Dan Sajjad <dan.sajjad@gmail.com>
2016-08-06 17:13:34 -0700

    Create skeleton

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+  console.log('Serving request for ' + req.url);
+  res.render('index');
+}); 
+
+app.listen(process.env.port || 3000, function() {
+  console.log('Jumpy_cat server listening on port 3000');
+});
